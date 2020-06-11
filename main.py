179363fb0ea397e95295982af71530882a2e0f8f
Rishi Kaushik <rishikaushik60@gmail.com>
2020-06-12 00:24:11 +0530

    Added code to thicken initial landmass

--- main.py
+++ main.py
@@ -92,6 +92,97 @@ def generate_initial_map():
     generated_map, successful = create_land(generated_map, hq_x, hq_y)
     while not successful:
         generated_map, successful = create_land(generated_map, hq_x, hq_y)
+
+    generated_map = add_land(generated_map)
+    return generated_map
+
+
+def generate_tile_info(generated_map, y, x):
+    tile_info = [y, x, [], 0]
+    if x + 1 < config_data['width'] and generated_map[y][x + 1] == terrain_stats['sea'][0]:
+        tile_info[3] += 1
+        tile_info[2].append('e')
+    if x - 1 > 0 and generated_map[y][x - 1] == terrain_stats['sea'][0]:
+        tile_info[3] += 1
+        tile_info[2].append('w')
+    if y + 1 < config_data['height'] and generated_map[y + 1][x] == terrain_stats['sea'][0]:
+        tile_info[3] += 1
+        tile_info[2].append('s')
+    if y - 1 > 0 and generated_map[y - 1][x] == terrain_stats['sea'][0]:
+        tile_info[3] += 1
+        tile_info[2].append('n')
+    return tile_info
+
+
+def add_land(generated_map):
+    target_land_count = int(config_data['height'] * config_data['width'] * float(config_data['land_ratio']))
+    current_land_count = 0
+
+    # generating a set of land tiles
+    land_tiles = []
+    for y in range(config_data['height']):
+        for x in range(config_data['width']):
+            if generated_map[y][x] == terrain_stats['plains'][0]:
+                current_land_count += 1
+                land_tiles.append(generate_tile_info(generated_map, y, x))
+                if land_tiles[-1][3] == 4:
+                    del land_tiles[-1]
+
+    for i in range(int((target_land_count - current_land_count)/2)):
+        while True:
+            target_tile_index = random.randint(0, len(land_tiles)-1)
+            target_tile = land_tiles[target_tile_index]
+            if target_tile[3] == 1 and random.uniform(0, 1) > 0:
+                append_direction = random.sample(target_tile[2], 1)[0]
+            elif target_tile[3] == 2 and random.uniform(0, 1) > .2:
+                append_direction = random.sample(target_tile[2], 1)[0]
+            elif target_tile[3] == 3 and random.uniform(0, 1) > .8:
+                append_direction = random.sample(target_tile[2], 1)[0]
+            elif target_tile[3] == 4 and random.uniform(0, 1) > 1:
+                append_direction = random.sample(target_tile[2], 1)[0]
+            else:
+                continue
+            if append_direction == 'n':
+                land_tiles[target_tile_index][2].remove('n')
+                land_tiles[target_tile_index][3] -= 1
+                if land_tiles[target_tile_index][3] == 4:
+                    del land_tiles[target_tile_index]
+
+                generated_map[target_tile[0] - 1][target_tile[1]] = terrain_stats['plains'][0]
+                land_tiles.append(generate_tile_info(generated_map, target_tile[0] - 1, target_tile[1]))
+                if land_tiles[-1][3] == 4:
+                    del land_tiles[-1]
+            if append_direction == 's':
+                land_tiles[target_tile_index][2].remove('s')
+                land_tiles[target_tile_index][3] -= 1
+                if land_tiles[target_tile_index][3] == 4:
+                    del land_tiles[target_tile_index]
+
+                generated_map[target_tile[0] + 1][target_tile[1]] = terrain_stats['plains'][0]
+                land_tiles.append(generate_tile_info(generated_map, target_tile[0] + 1, target_tile[1]))
+                if land_tiles[-1][3] == 4:
+                    del land_tiles[-1]
+            if append_direction == 'e':
+                land_tiles[target_tile_index][2].remove('e')
+                land_tiles[target_tile_index][3] -= 1
+                if land_tiles[target_tile_index][3] == 4:
+                    del land_tiles[target_tile_index]
+
+                generated_map[target_tile[0]][target_tile[1] + 1] = terrain_stats['plains'][0]
+                land_tiles.append(generate_tile_info(generated_map, target_tile[0], target_tile[1] + 1))
+                if land_tiles[-1][3] == 4:
+                    del land_tiles[-1]
+            if append_direction == 'w':
+                land_tiles[target_tile_index][2].remove('w')
+                land_tiles[target_tile_index][3] -= 1
+                if land_tiles[target_tile_index][3] == 4:
+                    del land_tiles[target_tile_index]
+
+                generated_map[target_tile[0]][target_tile[1] - 1] = terrain_stats['plains'][0]
+                land_tiles.append(generate_tile_info(generated_map, target_tile[0], target_tile[1] - 1))
+                if land_tiles[-1][3] == 4:
+                    del land_tiles[-1]
+            break
     return generated_map
 
 
@@ -107,29 +198,29 @@ def create_land(generated_map, hq_x, hq_y):
             if generated_map[y][x + 1] == terrain_stats['p2 stronghold'][0]:
                 break
             if generated_map[y][x + 1] == terrain_stats['sea'][0]:
-                next_square.append([y, x+1])
+                next_square.append([y, x + 1])
         if x - 1 > 0:
             if generated_map[y][x - 1] == terrain_stats['p2 stronghold'][0]:
                 break
             if generated_map[y][x - 1] == terrain_stats['sea'][0]:
-                next_square.append([y, x-1])
+                next_square.append([y, x - 1])
         if y + 1 < config_data['height']:
             if generated_map[y + 1][x] == terrain_stats['p2 stronghold'][0]:
                 break
             if generated_map[y + 1][x] == terrain_stats['sea'][0]:
-                next_square.append([y+1, x])
+                next_square.append([y + 1, x])
         if y - 1 > 0:
             if generated_map[y - 1][x] == terrain_stats['p2 stronghold'][0]:
                 break
             if generated_map[y - 1][x] == terrain_stats['sea'][0]:
-                next_square.append([y-1, x])
+                next_square.append([y - 1, x])
         if len(next_square) == 0:
             return map_backup, False
         y, x = random.sample(next_square, 1)[0]
-    # for _ in range(int(config_data['height'] * config_data['width'] * config_data['land_ratio'])):
     for i in range(config_data['height']):
         for j in range(config_data['width']):
-            if generated_map[i][j] == terrain_stats['p1 stronghold'][0] or generated_map[i][j] == terrain_stats['p2 stronghold'][0]:
+            if generated_map[i][j] == terrain_stats['p1 stronghold'][0] or generated_map[i][j] == \
+                    terrain_stats['p2 stronghold'][0]:
                 generated_map[i][j] = terrain_stats['plains'][0]
     return generated_map, True
 
